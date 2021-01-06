5dd43cf5b9fdb22f047129974e683ba5a3fdf07b
Pierre MORICHAU <pmorichau@eos-imaging.com>
2021-01-06 17:33:17 +0100

    Update files from xlsx to csv
    Change-Id: I0464e242e010196964a978839c068e5e33571d72

--- CCAM_my_app.py
+++ CCAM_my_app.py
@@ -2,15 +2,15 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df_2015 = pd.read_excel("Actes_techniques_de_la_CCAM_en_2015.xls", sep=r'\s*,\s*',)
+df_2015 = pd.read_csv("Actes_techniques_de_la_CCAM_en_2015.csv",delimiter=';')
 df_2015['Année'] = 2015
-df_2016 = pd.read_excel("Actes_techniques_de_la_CCAM_en_2016.xls", sep=r'\s*,\s*',)
+df_2016 = pd.read_csv("Actes_techniques_de_la_CCAM_en_2016.csv",delimiter=';')
 df_2016['Année'] = 2016
-df_2017 = pd.read_excel("Actes_techniques_de_la_CCAM_en_2017.xls", sep=r'\s*,\s*',)
+df_2017 = pd.read_csv("Actes_techniques_de_la_CCAM_en_2017.csv",delimiter=';')
 df_2017['Année'] = 2017
-df_2018 = pd.read_excel("Actes_techniques_de_la_CCAM_en_2018.xls", sep=r'\s*,\s*',)
+df_2018 = pd.read_csv("Actes_techniques_de_la_CCAM_en_2018.csv",delimiter=';')
 df_2018['Année'] = 2018
-df_2019 = pd.read_excel("Actes_techniques_de_la_CCAM_en_2019.xls", sep=r'\s*,\s*',)
+df_2019 = pd.read_csv("Actes_techniques_de_la_CCAM_en_2019.csv",delimiter=';')
 df_2019['Année'] = 2019
 
 frames = [df_2015,df_2016,df_2017, df_2018,df_2019]
@@ -31,5 +31,5 @@ st.dataframe(sub_result)
 
 
 # Use directly Columns as argument. You can use tab completion for this!
-fig = px.scatter(sub_result,x=sub_result['Année'], y=sub_result["Quantité_d_actes"],labels =sub_result["Sous-catégorie_d_acte"],color=sub_result['Code_Acte'])#, size=result["Montants_remboursés"])#,labels=result["Catégorie_d_acte"])
+fig = px.scatter(sub_result,x='Année', y="Quantité_d_actes")
 st.plotly_chart(fig) 
