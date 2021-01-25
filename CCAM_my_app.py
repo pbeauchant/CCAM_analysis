14f8edc84a9cefb3ba9c3b8da9e0e7fe04195d7e
pbeauchant <13273035+pbeauchant@users.noreply.github.com>
2021-01-25 09:36:01 +0100

    Update CCAM_my_app.py
    CAGR calculation

--- CCAM_my_app.py
+++ CCAM_my_app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import streamlit as st
 
@@ -29,7 +30,8 @@ st.markdown("***{}***".format(sub_result['Libellé_long'].iloc[0]))
 st.markdown('----')
 st.dataframe(sub_result)
 
-
+CAGR = np.round(((sub_result['Libellé_long'].iloc['Année'==2019]/sub_result['Libellé_long'].iloc['Année'==2015])^(1/4)-1)*100,2)
 # Use directly Columns as argument. You can use tab completion for this!
+st.markdown("***CAGR 2015-2019: {}***".format(CAGR))
 fig = px.scatter(sub_result,x='Année', y="Quantité_d_actes")
 st.plotly_chart(fig) 
